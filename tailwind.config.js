9a7ef99376c4a5751f24571dbe9eac9ba0459a7e
Ammar Shoeb <ammaryahia23@gmail.com>
2025-04-03 19:03:39 +0300

    w commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './**/*.html',
+      './**/*.js',
+      // Add any other file patterns where you use Tailwind classes
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    safelist: [{ pattern: /.*/ }],
+  }
