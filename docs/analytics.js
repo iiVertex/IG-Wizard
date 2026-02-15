a22a3315f49bd9860a85c4278d91f10d4283e0b9
Ammar Shoeb <ammaryahia23@gmail.com>
2026-02-15 20:55:52 +0300

    removed unnecessary files

--- docs/analytics.js
+++
@@ -1,8 +0,0 @@
-import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-// Initialize Vercel Analytics
-inject();
-
-// Initialize Vercel Speed Insights
-injectSpeedInsights();
